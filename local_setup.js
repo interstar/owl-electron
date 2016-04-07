3254cb382c78eaa1807e1a53eb15460fab5129b7
phil jones <interstar@gmail.com>
2016-04-07 20:59:46 -0300

    adding default pages directory

--- local_setup.js
+++ local_setup.js
@@ -1,13 +1,12 @@
 // local_setup.js 
 // ideally, all customizations for version live in here
 
-// This is the single-page localStorage version
-
+// This is the Electron version
 
 function localConfigs() {
 	setOutlinerPrefs ("#outliner", true, false);
 	opSetFont (appPrefs.outlineFont, appPrefs.outlineFontSize, appPrefs.outlineLineHeight); 	
-    pageStore = new BrowserBasedPageStore();
+    pageStore = new NodeBasedPageStore("./pages");
 }
 
 
